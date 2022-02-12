e1343b3646b7fc661b5bb6e3ca7b0534728d997a
aimenAlt <ymnghost@gmail.com>
2022-02-11 21:46:25 -0800

    added validations

--- src/main/java/com/csumb/cst363/controller/ControllerPrescription.java
+++ src/main/java/com/csumb/cst363/controller/ControllerPrescription.java
@@ -83,8 +83,25 @@ public class ControllerPrescription {
 	public String processFillForm(Prescription p, Model model) throws SQLException {
 
 
-		// TODO  
-		
+		// TODO
+
+		String selectRxidQuery = 	"SELECT rxid FROM prescription WHERE rxid = ?";
+		ArrayList<Object> selectRxidVars = new ArrayList<>();
+		selectRxidVars.add(p.getRxid());
+		ArrayList<Object> rxidResult = getFromDatabase(selectRxidQuery, selectRxidVars);
+		try {
+			CachedRowSet finalResult = (CachedRowSet) rxidResult.get(0);
+			System.out.print("finalResult: ");
+			System.out.println(finalResult.getInt(1));
+		} catch(Exception e) {
+			model.addAttribute("message", "Error: RXID does not exist");
+			model.addAttribute("prescription", p);
+			return "prescription_fill";
+		}
+
+
+
+
 		/* 
 		 * replace the following with code to validate the updated prescription data
 		 * and update database.
@@ -97,14 +114,16 @@ public class ControllerPrescription {
 			model.addAttribute("prescription", p);
 			return "prescription_fill";
 		}
+
 		String insertPharmacyQuery = "INSERT INTO pharmacy (pharmacy_name, street) VALUES (?,?)";
 		ArrayList<Object> insertPharmacyVars = new ArrayList<>();
 		insertPharmacyVars.add(p.getPharmacyName());
 		insertPharmacyVars.add(p.getPharmacyAddress());
 		Long newPharmacyId = (Long) insertIntoDatabase(insertPharmacyQuery, insertPharmacyVars).get(0);
 
-		String updatePrescriptionQuery = "UPDATE prescription SET fill_date = CURRENT_DATE WHERE rxid = ?";
+		String updatePrescriptionQuery = "UPDATE prescription SET fill_date = CURRENT_DATE, pharmacy_id = ? WHERE rxid = ?";
 		ArrayList<Object> updatePrescriptionVars = new ArrayList<>();
+		updatePrescriptionVars.add(newPharmacyId.intValue());
 		updatePrescriptionVars.add(p.getRxid());
 		insertIntoDatabase(updatePrescriptionQuery, updatePrescriptionVars);
 
@@ -118,7 +137,13 @@ public class ControllerPrescription {
 		ArrayList<Object> selectPrescriptionVars = new ArrayList<>();
 		selectPrescriptionVars.add(p.getRxid());
 		ArrayList<Object> queryResult = getFromDatabase(selectPrescriptionQuery, selectPrescriptionVars);
-		System.out.println("oxy");
+//		try {
+//			CachedRowSet result = (CachedRowSet) queryResult.get(0);
+//		} catch(Exception e) {
+//			model.addAttribute("message", "Error: RXID does not exist");
+//			model.addAttribute("prescription", p);
+//			return "prescription_fill";
+//		}
 		CachedRowSet result = (CachedRowSet) queryResult.get(0);
 		System.out.println(result.getString(3));
 
